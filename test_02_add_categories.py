7186df0cbb5694f620a0edbe8c87001ffb97c428
severinum <sewerynmichota@gmail.com>
2023-10-17 20:17:53 +0100

    Add cleanup methods

--- test_02_add_categories.py
+++ test_02_add_categories.py
@@ -7,19 +7,18 @@ import sys
 from configuration import *
 
 """
-    Testing if:
-        - Can register new user
-        - Longin as new user (plus: receiving JWT Token)
-        - Remove user account (plus: if user has role admin assigned)
+    The purpose of that test is to create all required by test post categories
+    It is: sport, politcs, health, tech
 """
 
 def test_ShoulCreateAdminAccountAndLoginAsAdmin_When_AdminAuthenticated():
-    # Generate random user email
+    ######## Log in as Admin and create categories ############
+    # Admin credentials
     userEmail = "testadmin@pythontest.com"
     userPlainPassword = 'test123xyz'
     userUsername = 'testadmin'
 
-    ########  Register New User (with admin role) ########
+    ########  Register admin ########
     registrationUrl = getHost() + "/users/register"
     username = userUsername
     email = userEmail
@@ -38,5 +37,52 @@ def test_ShoulCreateAdminAccountAndLoginAsAdmin_When_AdminAuthenticated():
     # Assert if response has code 201
     assert response.status_code == 201
     responseBody = response.json()
-    # user_id: will be used to remove test user
     user_id = responseBody['_id']
+
+    # Login as admin
+    url = getHost() + "/users/login"
+    headers = {'Content-Type': 'application/json'}
+    payload = {'email': email, 'password': password}
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    # Assert if response has code 200
+    assert response.status_code == 200
+    # Assert if response contain token
+    responseBody = response.json()
+    token = responseBody['token']
+    assert token is not None
+
+    # Add categories: sport, politcs, health, tech
+    # Add topic: sport
+    url = getHost() + "/topics"
+    headers = headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
+    payload = {
+        'name': 'sport'
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    # Assert if add item has code 201
+    assert response.status_code == 201
+
+    # Add topic: politics
+    payload = {
+        'name': 'politics'
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    # Assert if add item has code 201
+    assert response.status_code == 201
+
+    # Add topic: health
+    payload = {
+        'name': 'health'
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    # Assert if add item has code 201
+    assert response.status_code == 201
+
+    # Add topic: tech
+    payload = {
+        'name': 'tech'
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    # Assert if add item has code 201
+    assert response.status_code == 201
+
--- test_Z_Cleanup.py
+++ test_Z_Cleanup.py
@@ -8,40 +8,78 @@ from configuration import *
 
 
 """
-    Testing if:
-        - Can register new user
-        - Longin as new user (plus: receiving JWT Token)
-        - Remove user account (plus: if user has role admin assigned)
+DB cleaning functions
 """
 
-def test_ShouldRemoveAdminAccount_When_ValidUserId():
-    ######## Login as user ########
-    # Admin user was created in file test_02_add_categories.py
+def test_ShouldEmptyTopicsCollection_When_AdminAuthenticated():
+    ######## Log in as Admin and create categories ############
+    # Admin credentials
+    email = "testadmin@pythontest.com"
+    password = 'test123xyz'
+    username = 'testadmin'
 
+    # Login as admin
     url = getHost() + "/users/login"
     headers = {'Content-Type': 'application/json'}
-    payload = {'email': "testadmin@pythontest.com", 'password': 'test123xyz'}
+    payload = {'email': email, 'password': password}
     response = requests.post(url, headers=headers, data=json.dumps(payload))
     # Assert if response has code 200
     assert response.status_code == 200
     # Assert if response contain token
     responseBody = response.json()
-    jwtToken = responseBody['token']
-    assert jwtToken is not None
-    # Assert if token was decoded
-    jwtDecoded = jwt.decode(jwtToken, TOKEN_SECRET, algorithms=['HS512'])
-    assert jwtDecoded is not None
-    # Assert if token contain admin role
-    userRoles = jwtDecoded['user_roles']
-    userId = jwtDecoded['user_id']
-    assert 'admin' in userRoles
+    token = responseBody['token']
+    assert token is not None
 
-    ######## Remove User (to maintain clean database, validate admin role for delete endpoint) ########
-    url = getHost() + "/users/" + userId
-    headers = {'Authorization': 'Bearer ' + jwtToken}
-    response = requests.delete(url, headers=headers)
-    # Assert if response has code 200
+    # Empty topics collection
+    url = getHost() + "/posts/all"
+    headers = headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
+    response = requests.delete(url, headers=headers, data=json.dumps(payload))
+    # Assert if add item has code 200
     assert response.status_code == 200
-    responseBody = response.json()
-    assert responseBody['message'] == 'User deleted : ' + userId
+
+    # Empty topics collection
+    url = getHost() + "/topics/all"
+    headers = headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
+    response = requests.delete(url, headers=headers, data=json.dumps(payload))
+    # Assert if add item has code 200
+    assert response.status_code == 200
+
+    # Empty users collection
+    url = getHost() + "/users/all"
+    headers = headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
+    response = requests.delete(url, headers=headers, data=json.dumps(payload))
+    # Assert if add item has code 200
+    assert response.status_code == 200
+
+
+# def test_ShouldRemoveAdminAccount_When_ValidUserId():
+#     ######## Login as user ########
+#     # Admin user was created in file test_02_add_categories.py
+
+#     url = getHost() + "/users/login"
+#     headers = {'Content-Type': 'application/json'}
+#     payload = {'email': "testadmin@pythontest.com", 'password': 'test123xyz'}
+#     response = requests.post(url, headers=headers, data=json.dumps(payload))
+#     # Assert if response has code 200
+#     assert response.status_code == 200
+#     # Assert if response contain token
+#     responseBody = response.json()
+#     jwtToken = responseBody['token']
+#     assert jwtToken is not None
+#     # Assert if token was decoded
+#     jwtDecoded = jwt.decode(jwtToken, TOKEN_SECRET, algorithms=['HS512'])
+#     assert jwtDecoded is not None
+#     # Assert if token contain admin role
+#     userRoles = jwtDecoded['user_roles']
+#     userId = jwtDecoded['user_id']
+#     assert 'admin' in userRoles
+
+#     ######## Remove User (to maintain clean database, validate admin role for delete endpoint) ########
+#     url = getHost() + "/users/" + userId
+#     headers = {'Authorization': 'Bearer ' + jwtToken}
+#     response = requests.delete(url, headers=headers)
+#     # Assert if response has code 200
+#     assert response.status_code == 200
+#     responseBody = response.json()
+#     assert responseBody['message'] == 'User deleted : ' + userId
 
