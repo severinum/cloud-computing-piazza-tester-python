e91c86860bffb5c6bbdb6ee0637f40f13c37890e
severinum <sewerynmichota@gmail.com>
2023-10-20 18:59:00 +0100

    Add Prod and Dev envs

--- configuration.py
+++ configuration.py
@@ -1,6 +1,13 @@
-PORT=3000
-HOST='localhost'
-TOKEN_SECRET='ICtxGV?SH1W^0QfxJczj4Qw8TrxdtPg8y7TvVqs$1HnnJFNz6GL_q_MtS8'
+#### Local Environment
+#PORT=3000
+#HOST='localhost'
+
+#### Production Environment (K8S Cluster GCP)
+HOST='34.41.98.117'
+PORT=80
+
+
+TOKEN_SECRET='ICtxGVsSH1W40QfxJczj4Qw8TrxdtPg8y7TvVqsa1HnnJFNz6GL_q_MtS8'
 
 
 def getHost():
